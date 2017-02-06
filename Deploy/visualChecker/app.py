c53144694c0735826135d3f639d178d80170a432
swang <Shuai.Wang@verisk.com>
2017-02-06 16:16:48 -0500

    Add rainy or sunny

--- Deploy/visualChecker/app.py
+++ Deploy/visualChecker/app.py
@@ -5,24 +5,34 @@ import tornado.wsgi
 import tornado.httpserver
 
 import sys
-import re
-import os
+
+import yweather
 reload(sys)
 sys.setdefaultencoding('utf8')
 #########################################
 # Obtain the flask app object
 app = flask.Flask(__name__)
-
+client = yweather.Client()
 @app.route('/')
 def index():
+    # Starting point
     return flask.render_template('index.html', has_result=False)
 
 @app.route('/run_model', methods=['POST'])
 def run_model():
-    result_1="raw"
+    text_input = flask.request.form['link_input']
+    if "San Francisco" in text_input:
+        imgFile = "rainy.png"
+    else:
+        imgFile = "sunny.jpeg"
+    ### Place your function here
+    output = changeText(text_input)
     return flask.render_template('index.html', has_result=True,
-            result=(True, result_1,
-                    ))
+            result=(True, text_input,
+                    text_input,
+                    output,
+    imgFile                  ))
+
 def start_tornado(app, port=""):
     http_server = tornado.httpserver.HTTPServer(
         tornado.wsgi.WSGIContainer(app))
@@ -30,6 +40,10 @@ def start_tornado(app, port=""):
     print("Tornado server starting on port {}".format(port))
     tornado.ioloop.IOLoop.instance().start()
 
+def changeText(input):
+    id = client.fetch_woeid(input)
+
+
 
 if __name__ == '__main__':
-    start_tornado(app, port=5011)
+    start_tornado(app, port=5012)
